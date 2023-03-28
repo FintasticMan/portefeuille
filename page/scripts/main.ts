9ea010754afeaab7a4c1a5c50ca9f82a6ff3ca80
Finlay Davidson <finlay.davidson@coderclass.nl>
2023-03-29 00:37:03 +0200

    scripts: Clean up functions and emit sourcemaps

--- page/scripts/main.ts
+++ page/scripts/main.ts
@@ -1,29 +1,36 @@
-window.addEventListener("load", () => {
+function colourPages() {
     const colours: string[] = ["#84CAE7", "#4CE0D2", "#22AAA1"];
     const pages = document.querySelectorAll<HTMLElement>(".page");
     for (let i = 0; i < pages.length; i++) {
         pages[i].style.backgroundColor = colours[i % colours.length];
     }
+}
 
+function clickHamburger() {
+    const header = document.querySelector<HTMLElement>("#header");
+    if (header === null) {
+        return;
+    }
+    const menu = header.querySelector<HTMLElement>(".navbar");
+    if (menu === null) {
+        return;
+    }
+
+    menu.classList.toggle("show");
+
+    if (header.classList.toggle("expanded")) {
+        const height = menu.offsetHeight;
+        header.style.height = height + "px";
+    } else {
+        header.style.height = "";
+    }
+}
+
+function addHamburgerListener() {
     const hamburger = document.querySelector("#hamburger");
 
-    hamburger?.addEventListener("click", () => {
-        const header = document.querySelector<HTMLElement>("#header");
-        if (header === null) {
-            return;
-        }
-        const menu = header.querySelector<HTMLElement>(".navbar");
-        if (menu === null) {
-            return;
-        }
+    hamburger?.addEventListener("click", clickHamburger);
+}
 
-        menu.classList.toggle("show");
-
-        if (header.classList.toggle("expanded")) {
-            const height = menu.offsetHeight;
-            header.style.height = height + "px";
-        } else {
-            header.style.height = "";
-        }
-    });
-});
+window.addEventListener("load", colourPages);
+window.addEventListener("load", addHamburgerListener);
