655b0d067b33e074ca909718570789b7d143f910
Finlay Davidson <finlay.davidson@coderclass.nl>
2023-03-29 00:22:27 +0200

    styling: Add hamburger menu

--- page/scripts/main.ts
+++ page/scripts/main.ts
@@ -1,10 +1,29 @@
-const colours: string[] = ["#84CAE7", "#4CE0D2", "#22AAA1"];
-
-function colourPages(): void {
+window.addEventListener("load", () => {
+    const colours: string[] = ["#84CAE7", "#4CE0D2", "#22AAA1"];
     const pages = document.querySelectorAll<HTMLElement>(".page");
     for (let i = 0; i < pages.length; i++) {
         pages[i].style.backgroundColor = colours[i % colours.length];
     }
-}
 
-window.onload = colourPages;
+    const hamburger = document.querySelector("#hamburger");
+
+    hamburger?.addEventListener("click", () => {
+        const header = document.querySelector<HTMLElement>("#header");
+        if (header === null) {
+            return;
+        }
+        const menu = header.querySelector<HTMLElement>(".navbar");
+        if (menu === null) {
+            return;
+        }
+
+        menu.classList.toggle("show");
+
+        if (header.classList.toggle("expanded")) {
+            const height = menu.offsetHeight;
+            header.style.height = height + "px";
+        } else {
+            header.style.height = "";
+        }
+    });
+});
