54a5d44b1bfc9bcd1d1973dd3a1ca54312e9255c
Finlay Davidson <finlay.davidson@coderclass.nl>
2023-03-29 22:03:05 +0200

    all: Add social icons + logo + incorporate feedback

--- page/scripts/main.ts
+++ page/scripts/main.ts
@@ -1,9 +1,9 @@
 function colourPages() {
     let colours: string[];
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        colours = ["#289ACB", "#C85475", "#17726C", "#6D2F90", "#C5B359"]
+        colours = ["#289ACB", "#C85475", "#17726C", "#29274C", "#C5B359"];
     } else {
-        colours = ["#84CAE7", "#EABFCB", "#22AAA1", "#9E56C8", "#DFD5A5"];
+        colours = ["#84CAE7", "#EABFCB", "#22AAA1", "#514C94", "#DFD5A5"];
     }
     const pages = document.querySelectorAll<HTMLElement>(".page");
     for (let i = 0; i < pages.length; i++) {
@@ -25,17 +25,32 @@ function clickHamburger() {
 
     if (header.classList.toggle("expanded")) {
         const height = menu.offsetHeight;
-        header.style.height = height + "px";
+        header.style.height = height.toString() + "px";
     } else {
         header.style.height = "";
     }
 }
 
 function addHamburgerListener() {
-    const hamburger = document.querySelector("#hamburger");
+    const hamburger = document.getElementById("hamburger");
 
     hamburger?.addEventListener("click", clickHamburger);
 }
 
+function calcAge(date: Date) {
+    const now = Date.now();
+    const diff = now - date.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.2425));
+}
+
+function updateAge() {
+    const elem = document.getElementById("age");
+    if (elem === null) {
+        return;
+    }
+    elem.innerHTML = calcAge(new Date(2005, 7, 6)).toString();
+}
+
 window.addEventListener("load", colourPages);
 window.addEventListener("load", addHamburgerListener);
+window.addEventListener("load", updateAge);
